058bf1318d8a84be6c6dd9b6021207da90f64ea6
hexagon14 <hexagon14@mail.ru>
2021-10-27 15:15:50 +0300

    Testing ServiceInterface

---
+++ app/Contracts/ImageServiceInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\ImageService;
+
+Interface ImageServiceInterface
+{
+    public function upload($request);
+}
--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,7 +12,7 @@ class ImageController extends Controller
     /**
      * @var ImageService
      */
-    protected $service;
+    protected ImageService $service;
 
     /**
      * @param ImageService $imageService
